6ca5b63753e713fe25b8c498832b4bbd5d0d1464
cap <cap@ubuntu80>
2021-11-25 10:03:17 +1100

    server side code initialised.

---
+++ src/main/java/com/bigcloudies/servermanager/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bigcloudies.servermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigcloudies/servermanager/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcloudies.servermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
