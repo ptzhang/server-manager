0114a4809bb6dba8cd0398d453af9849aeee95a0
cap <cap@ubuntu80>
2021-11-25 12:10:27 +1100

    server side code finalised.

--- src/main/java/com/bigcloudies/servermanager/ServerManagerApplication.java
+++ src/main/java/com/bigcloudies/servermanager/ServerManagerApplication.java
@@ -25,9 +25,9 @@ public class ServerManagerApplication {
 	@Bean
 	CommandLineRunner run(ServerRepo serverRepo) {
 		return args -> {
-			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/image/server2.png", SERVER_DOWN));
-			serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.0.83", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+			serverRepo.save(new Server(null, "127.0.0.1", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.15", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
 		};
 	}
